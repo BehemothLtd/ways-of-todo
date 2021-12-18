150eef11da82bedfa8e00b05796b8a9dc7e52434
bachdx2812 <bachdx.hut@gmail.com>
2021-12-18 17:29:35 +0700

    init svelte

---
+++ svelte/src/App.svelte
@@ -0,0 +1,51 @@
+<script>
+  import TodoItem from "./TodoItem.svelte";
+
+  let text = "abc";
+
+  let todos = [
+    {
+      content: "Learn Svelte",
+      done: true,
+    },
+    {
+      content: "Learn TypeScript",
+      done: false,
+    },
+  ];
+
+  let newTodo = null;
+
+  function createNewTodo() {
+    todos.push({
+      content: newTodo,
+      done: false,
+    });
+
+    // Because Svelte's reactivity is based on assignments,
+    // using array methods like .push() and .splice() won't automatically trigger updates.
+    todos = todos;
+
+    // clear newTodo
+    newTodo = null;
+  }
+</script>
+
+<main>
+  <h1>Todo Svelte!</h1>
+  {text}
+  <ul class="list-group">
+    {#each todos as item}
+      <TodoItem {item} bind:text />
+    {/each}
+  </ul>
+
+  <div>
+    <input type="text" bind:value={newTodo} />
+    <button on:click={() => createNewTodo()}>Add</button>
+  </div>
+</main>
+
+<style>
+  @import "https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css";
+</style>
---
+++ svelte/src/TodoItem.svelte
@@ -0,0 +1,19 @@
+<script>
+  export let item;
+  export let text;
+
+  function markAsDone() {
+    item.done = true;
+    text = "new test";
+  }
+</script>
+
+<li class="list-group-item {item.done ? 'bg-secondary' : ''}">
+  {item.content}
+
+  {#if !item.done}
+    <button type="button" class="btn btn-success" on:click={() => markAsDone()}
+      >Mark as done</button
+    >
+  {/if}
+</li>
